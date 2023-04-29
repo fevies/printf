dcd21873494c3c58d47e31432282505c5c882b6a
fevies <elviesodhiambo@gmail.com>
2023-04-28 17:38:52 -0700

    a

--- main.h
+++ main.h
@@ -5,22 +5,22 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct specifiers
+typedef struct sps
 {
-	char specifiers;
+	char sps;
 	int (*f)(va_list);
 } specifiers_t;
 
-int get_function(char s, va_list args);
+int firstfunc(char s, va_list args);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
 
-int print_digit(va_list args);
-int print_mod(va_list args);
-int print_rev_string(va_list args);
+int dgt(va_list args);
+int mod(va_list args);
+int rev_string(va_list args);
 int print_char(va_list args);
-int print_string(va_list args);
+int string(va_list args);
 
 
 #endif
